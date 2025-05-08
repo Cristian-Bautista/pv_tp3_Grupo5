9d57576050f58aaa7b1d5a4a417d67726c85da35
Cristian-Bautista <cristianbautista1516@gmail.com>
2025-05-08 00:52:31 -0300

    ejercicio 5 echo [cristian bautista]

--- tp3-pv-grupo5/src/js/los-puntos-del-ejercicio-adicional.js
+++ tp3-pv-grupo5/src/js/los-puntos-del-ejercicio-adicional.js
@@ -1,3 +1,4 @@
+//Punto 1
 export const mostrarproductos = (productos) =>{
     productos.forEach(({Descripcion, Precio})=>{
         console.log(`
@@ -5,4 +6,9 @@ export const mostrarproductos = (productos) =>{
             Precio: ${Precio}
         `)
     })
+}
+//Punto 5
+export const agregarproducto = (productos) =>{
+    productos.push({ descripcion: "Samsung Galaxy S25 Ultra 512gb", precio: 2987288 });
+    console.log(productos);
 }
